517b5c5ec65fe1d614ec79339d03b01db6e8bcfe
beginner64 <ankitpal064@gmail.com>
2021-06-16 23:01:09 +0900

    progress

--- python/ds/adt/queue/implement_queue_using_array.py
+++ python/ds/adt/queue/implement_queue_using_array.py
@@ -6,38 +6,68 @@ class QueueUsingArray(Queue):
         self.head = -1
         self.tail = -1
         self.size = 0
+        self.length =length
     
     def enqueue(self,value):
-
+        if self.full():
+            print("queue is full")
+            return
+        if self.empty():
+            self.head = (self.head+1)  % self.length
+        self.tail = (self.tail+ 1) % self.length
+        self.arr[self.tail] = value
+        self.size+=1
         
     
     def dequeue(self):
-        
+        if self.empty():
+            print("queue is empty")
+            return        
+        data = self.arr[self.head]
+        self.head = (self.head+1)  % self.length
+        self.size-=1
+        return data
     
     def empty(self):
-        return self.lst.empty()
+        return self.size == 0 
+    def full(self):
+        return self.size == self.length
 
-    # def print(self):
+    def print(self):
+        tmp = self.head
+        size = self.size
+        a=[]
+        while size >0:
+            a.append(self.arr[tmp])
+            tmp= (tmp+1)  % self.length
+            size-=1
+        print(a)
         
 
 if __name__ == "__main__":
-    queue = QueueUsingLinkedList()  
-    queue.enqueue(1)
-    queue.enqueue(2)
-    # queue.print()
-    queue.dequeue()
-    queue.dequeue()
-    # queue.print()
+    queue = QueueUsingArray(4)  
     queue.enqueue(1)
     queue.enqueue(2)
     queue.enqueue(3)
     queue.enqueue(4)
+    queue.print()
+    queue.dequeue()
+    queue.dequeue()
+    queue.dequeue()
+    queue.dequeue()
+    queue.dequeue()
+    queue.print()
+    queue.enqueue(1)
+    queue.print()
+    # queue.enqueue(2)
+    # queue.enqueue(3)
+    # queue.enqueue(4)
     # queue.print()
-    queue.dequeue()
-    queue.dequeue()
-    queue.enqueue(3)
-    queue.enqueue(4)
-    queue.enqueue(3)
-    queue.enqueue(4)
+    # queue.dequeue()
+    # queue.dequeue()
+    # queue.enqueue(3)
+    # queue.enqueue(4)
+    # queue.enqueue(3)
+    # queue.enqueue(4)
     # queue.print()
 
